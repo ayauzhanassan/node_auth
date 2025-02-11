f9167064da5d0297d4479c844faa1201aacbefd6
nero <aidyn.kazhakhmet@nu.edu.kz>
2025-02-11 12:37:19 +0500

    root route

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,10 @@ const User = require("../models/User");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.render("login", { error: null });
+});
+
 // Render Register Page with Errors
 router.get("/register", (req, res) => {
     res.render("register", { error: null });
